5d3a0995ba88b63644baa2f73426f6b211cec47f
earthmover009 <ankitlamba009@gmail.com>
2023-12-08 16:04:21 +0530

    created project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './components/recipes/recipes.component';
+import { RecipieListComponent } from './components/recipes/recipie-list/recipie-list.component';
+import { RecipieDetailComponent } from './components/recipes/recipie-detail/recipie-detail.component';
+import { RecipieItemComponent } from './components/recipes/recipie-list/recipie-item/recipie-item.component';
+import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './components/shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipieListComponent,
+    RecipieDetailComponent,
+    RecipieItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
